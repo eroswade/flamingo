2a3216282021d81495abf8332fa9f4c2df9cd2d7
eros <eros.wade@gmail.com>
2020-05-13 19:53:38 +0800

    完成PYTHON接口测试, 登录成功

--- python/IUSocket.py
+++ python/IUSocket.py
@@ -14,13 +14,11 @@ class IUSocket():
 
         d = 1
         d = d.to_bytes(1, 'big')
-        values = (d,originlen,complen)
-        s = struct.Struct('cII') #  I4: unsigned int s: char* f:float p:char* c: char i: int
-        packed_data = s.pack(*values)
+        packed_data = d + originlen.to_bytes(4,'little') + complen.to_bytes(4,'little')
         # unpacked_data = s.unpack(packed_data)
 
-        #b'\x01\x00\x00\x00a\x00\x00\x00^\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00x\x9cc``H<s\x86\x81\x81\xf9\x15\x03\x10\x04U+\x95\x16\xa7\x16\xe5%\xe6\xa6*Y)(\x19\x9a\x1b\x9b\x9a[\x9a\x18\x19\x9a\x19+\xe9((\x15$\x16\x17\x97\xe7\x17\xa5\x80\xa4\x8aK\xf3\x8a3\xf2\xcbS\x8b@\x12\xc99\x99\xa9y%%\x95\x05 ]\x86@\x81\xe2\x92\xc4\x92\xd2b\x10\xa7\x16\x00&\xf9\x1c]'
-        self.m_strSendBuf=packed_data + b'\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc' + compressed_data
+        # b"\x01a\x00\x00\x00^\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00x\x9cc``H<s\x86\x81\x81\xf9\x15\x03\x10\x04U+\x95\x16\xa7\x16\xe5%\xe6\xa6*Y)(\x19\x1a\x9b\x9a\x9bZZ\x18\x19\x9a\x19+\xe9((\x15$\x16\x17\x97\xe7\x17\xa5\x80\xa4\x8aK\xf3\x8a3\xf2\xcbS\x8b@\x12\xc99\x99\xa9y%%\x95\x05 ]\x86@\x81\xe2\x92\xc4\x92\xd2b\x10\xa7\x16\x00'k\x1c_"
+        self.m_strSendBuf=packed_data + b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' + compressed_data
         # print(zlib.decompress(compressed_data))
         self._send()
 
@@ -30,17 +28,11 @@ class IUSocket():
         # print('connection ')
 
     def _send(self):
-        ret = self.con.send(self.m_strSendBuf)
-        poem=''
-        while True:
-            data = self.con.recv(4)
-            if data:
-            #     self.con.close()
-            #     break
-                poem += data
-                print(poem)
+        ret = self.con.sendall(self.m_strSendBuf)
+        data = self.con.recv(1024)
 
         print('received: ')
+        print(data)
 
         # data = self.con.recv(1024)
         # print('recv:', data.decode())
--- python/Test.py
+++ python/Test.py
@@ -2,27 +2,27 @@ import protocolstream
 import json
 from IUSocket import IUSocket
 
-# ## 登录测试
-# msg_type_login=1002
-# # 构造指令
-# str = json.dumps({'username':'17357942163','password':'sunshower','clienttype':1,'status':1})
-# bs = protocolstream.BinaryStreamWriter()
-# bs.SendBufConstruct(msg_type_login,0,str)
-# bs.Flush()
-#
-# # socket
-# sock = IUSocket()
-# sock.Connect()
-# sock.Send(bs.m_data)
-
-## 心跳测试
-msg_type_beat=1000
+## 登录测试
+msg_type_login=1002
 # 构造指令
+str = json.dumps({'username':'13575982163','password':'sunshower','clienttype':1,'status':1})
 bs = protocolstream.BinaryStreamWriter()
-bs.SendBufConstruct(msg_type_beat,0,'')
+bs.SendBufConstruct(msg_type_login,0,str)
 bs.Flush()
 
 # socket
 sock = IUSocket()
 sock.Connect()
 sock.Send(bs.m_data)
+
+# ## 心跳测试
+# msg_type_beat=1000
+# # 构造指令
+# bs = protocolstream.BinaryStreamWriter()
+# bs.SendBufConstruct(msg_type_beat,0,'')
+# bs.Flush()
+#
+# # socket
+# sock = IUSocket()
+# sock.Connect()
+# sock.Send(bs.m_data)
